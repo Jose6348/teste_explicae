e2ffe7d5fd26b6e96b65214c087b42c7d538805d
Jose6348 <jfalasco88@gmail.com>
2025-03-04 16:24:54 -0300

    Index

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,30 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <TopBar />
+    <NavBar />
+    <CourseContent />
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TopBar from './components/TopBar.vue'
+import NavBar from './components/Navbar.vue'
+import CourseContent from './components/CourseContent.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    TopBar,
+    NavBar,
+    CourseContent
   }
 }
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
